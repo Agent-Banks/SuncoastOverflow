2621e1c1492e196eb8f82d7323e4a79d8ec5c3ae
Cody Banks <codydouglasbanks@gmail.com>
2020-07-07 18:01:15 -0400

    created state for questsions and and mapped to list

--- ClientApp/src/pages/Questions.jsx
+++ ClientApp/src/pages/Questions.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+function SingleQuestionForList(props) {
+  return (
+    <Link to="/questions/42" className="list-group-item list-group-item-action">
+      <div className="d-flex w-100 justify-content-between">
+        <h5 className="mb-1">{props.question.title}</h5>
+        <small>12 Answers</small>
+      </div>
+      <p className="mb-1">70 Votes</p>
+      <small className="mr-3">
+        <button className="btn btn-success btn-sm">
+          <span className="mr-2" role="img" aria-label="upvote">
+            👍🏻
+          </span>
+          50
+        </button>
+      </small>
+      <small className="mr-3">
+        <button className="btn btn-danger btn-sm">
+          <span className="mr-2" role="img" aria-label="downvote">
+            👎🏻
+          </span>{' '}
+          20
+        </button>
+      </small>
+    </Link>
+  )
+}
+
 export function Questions() {
+  const [questions, setQuestions] = useState([])
   return (
     <>
       <nav aria-label="breadcrumb">
@@ -15,64 +44,9 @@ export function Questions() {
         </ol>
       </nav>
       <div className="list-group">
-        <Link
-          to="/questions/42"
-          className="list-group-item list-group-item-action"
-        >
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">
-              How to access the correct `this` inside a callback?
-            </h5>
-            <small>12 Answers</small>
-          </div>
-          <p className="mb-1">70 Votes</p>
-          <small className="mr-3">
-            <button className="btn btn-success btn-sm">
-              <span className="mr-2" role="img" aria-label="upvote">
-                👍🏻
-              </span>
-              50
-            </button>
-          </small>
-          <small className="mr-3">
-            <button className="btn btn-danger btn-sm">
-              <span className="mr-2" role="img" aria-label="downvote">
-                👎🏻
-              </span>{' '}
-              20
-            </button>
-          </small>
-        </Link>
-
-        <Link
-          to="/questions/42"
-          className="list-group-item list-group-item-action"
-        >
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">
-              What is the difference between client-side and server-side
-              programming?
-            </h5>
-            <small>4 Answers</small>
-          </div>
-          <p className="mb-1">90 Votes</p>
-          <small className="mr-3">
-            <button className="btn btn-success btn-sm">
-              <span className="mr-2" role="img" aria-label="upvote">
-                👍🏻
-              </span>
-              85
-            </button>
-          </small>
-          <small className="mr-3">
-            <button className="btn btn-danger btn-sm">
-              <span className="mr-2" role="img" aria-label="downvote">
-                👎🏻
-              </span>{' '}
-              5
-            </button>
-          </small>
-        </Link>
+        {questions.map(question => (
+          <SingleQuestionForList question={question} />
+        ))}
       </div>
     </>
   )
