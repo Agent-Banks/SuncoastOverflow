213a167a03df4eceab1ccaa544d7022eece7adfb
Cody Banks <codydouglasbanks@gmail.com>
2020-07-07 20:09:02 -0400

    added required to question model

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SuncoastOverflow.Models
 {
     public class Question
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Body { get; set; }
         public string Tags { get; set; }
     }
